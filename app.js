fd7c62462da5dfa58dc36eee87e109684bc396f0
Carlos Díaz <Aprendiz.sena@pch-sbo.com>
2026-06-11 08:12:04 -0500

    Configurando el archivo app.js para que funcione en producción

--- app.js
+++ app.js
@@ -27,30 +27,6 @@ app.use(express.urlencoded({ extended: true }));
 // Logger HTTP
 app.use(morgan('combined'));
 
-/* const connectDB = async () => {
-  if (isConnected) return;
-
-  await mongoose.connect(process.env.MONGODB_URL, {
-    bufferCommands: false,
-    family: 4, // 👈 fix para Vercel
-    serverSelectionTimeoutMS: 10000,
-  });
- 
-  isConnected = true;
-  console.log('✅ Base de datos conectada');
-}; */
-
-// Middleware que conecta ANTES de cada request
-/* app.use(async (req, res, next) => {
-  try {
-    await connectDB();
-    next();
-  } catch (err) {
-    console.error('❌ Error conectando DB:', err);
-    res.status(500).json({ error: 'Error de conexión a la base de datos' });
-  }
-}); */
-
 // Rutas Públicas
 app.get('/api/health', (req, res) => {
   res.json({
@@ -66,13 +42,15 @@ app.use("/api/proveedor", proveedores);
 
 
 // INICIAR SERVIDOR
-app.listen(PORT, () => {
-  console.log('Servidor Express iniciando');
-  console.log('='.repeat(50));
-  console.log(`Puerto ${PORT}`);
-  console.log(`Base de datos SharePoint`)
-  console.log(`CORS: ${process.env.FRONTEND_URL_PRODUCCION || process.env.URL_PRUEBAS}`);
-  
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log('Servidor Express iniciando');
+    console.log('='.repeat(50));
+    console.log(`Puerto ${PORT}`);
+    console.log(`Base de datos SharePoint`)
+    console.log(`CORS: ${process.env.FRONTEND_URL_PRODUCCION || process.env.URL_PRUEBAS}`);
+    
+  });
+}
 
 export default app;
