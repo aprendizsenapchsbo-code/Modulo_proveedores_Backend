6b4f75ed16911a995e3a2b1a08f5b1573de63f41
Carlos Díaz <Aprendiz.sena@pch-sbo.com>
2026-06-11 14:39:48 -0500

    Cambiando archivos para que funcione con link del frontend desplegado en WordPress

--- services/emailServiceActualizacion.js
+++ services/emailServiceActualizacion.js
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 
 // Helper para construir URLs seguras
 const getUrlFrontend = (ruta) => {
-    const base = process.env.FRONTEND_URL_PRODUCCION || 'https://www.proveedor.pch-sbo.com/gestion-proveedores/#/';
+    const base = process.env.FRONTEND_URL_PRODUCCION || 'https://www.proveedor.pch-sbo.com';
     return `${base.replace(/\/$/, '')}/${ruta.replace(/^\//, '')}}`
 }
 
